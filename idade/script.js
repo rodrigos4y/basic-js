e434e44adb0e310ab90350d85a8636c94497ef73
Freitas <RODRIGO.FREITAS@sicpa.com>
2020-03-17 09:24:55 -0300

    Primeiros exercícios de javascript

---
+++ idade/script.js
@@ -0,0 +1,19 @@
+let idade = prompt("Quantos anos você tem?");
+
+function calculaDiasDeVida(idade){
+    let diasDeVida = idade * 365;   
+    return diasDeVida;
+}
+
+function calculaBatimentos(dias){
+    let batimentos =dias * 24 * 60 * 80;
+    return batimentos;
+}
+
+let dias = calculaDiasDeVida(idade);
+let batimentosCardiacos = calculaBatimentos(dias);
+
+
+document.write("Você já viveu " + dias + " dias.");
+document.write("Seu coração bateu " + batimentosCardiacos + " vezes até agora.");
+
