dd1a4793d747cb4f92200cd3d6d677cdd7406171
Rodrigo Freitas <RODRIGO.FREITAS@ceptis.com.br>
2020-03-26 17:03:00 -0300

    Refatoração
    - Utilizando o símbolo da crase para colocar textos e variáveis sem precisar de concatenação
    - Utilizando spread e a função Math.min na função para achar o menor número em um array

--- idade/script.js
+++ idade/script.js
@@ -14,6 +14,6 @@ let dias = calculaDiasDeVida(idade);
 let batimentosCardiacos = calculaBatimentos(dias);
 
 
-document.write("Você já viveu " + dias + " dias.");
+document.write(`Você já viveu ${dias} dias.`);
 document.write("Seu coração bateu " + batimentosCardiacos + " vezes até agora.");
 
